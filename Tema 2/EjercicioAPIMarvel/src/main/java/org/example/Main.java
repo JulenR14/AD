09abb594384a25d0fd011d5f0985c49790340172
JulenR14 <julen20191414@gmail.com>
2023-10-26 20:35:05 +0200

    Cambio url

--- Tema 2/EjercicioAPIMarvel/src/main/java/org/example/Main.java
+++ Tema 2/EjercicioAPIMarvel/src/main/java/org/example/Main.java
@@ -10,15 +10,19 @@ import java.security.NoSuchAlgorithmException;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws NoSuchAlgorithmException {
+        /*
         String password = "12374f4c3617a5ab259248791a3a144b2bf6976cb3276cd0ae7e143e6f8964fad74e7647f";
-
-        String urlWeb = "https://gateway.marvel.com/v1/public/comics?ts=1&apikey=3276cd0ae7e143e6f8964fad74e7647f&hash=cc3bfd21077c57373627025c432be8aa";
-
         MessageDigest md = MessageDigest.getInstance("MD5");
         byte[] digest = md.digest(password.getBytes(StandardCharsets.UTF_8));
         String md5 = DatatypeConverter.printHexBinary(digest);
 
         // imprimir resumen de mensaje MD5
         System.out.println(md5.toLowerCase());
+        */
+
+        String urlWeb = "https://gateway.marvel.com/v1/public/characters?ts=1&apikey=3276cd0ae7e143e6f8964fad74e7647f&hash=cc3bfd21077c57373627025c432be8aa";
+
+
+
     }
 }
