bda86395e592b5ae7262d1a0f2cb5fc9b05cb658
JulenR14 <julen20191414@gmail.com>
2023-11-09 20:36:34 +0100

    Solo falta el ultimo

--- Tema 3/Ejercicio6_JDBCySQLite/src/main/java/org/example/ConectarConBaseDatos.java
+++ Tema 3/Ejercicio6_JDBCySQLite/src/main/java/org/example/ConectarConBaseDatos.java
@@ -3,19 +3,14 @@ package org.example;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class ConectarConBaseDatos {
 
     private static Path rutaBaseDatos = Path.of("src","main", "resources" , "db", "f12006sqlite.db");
 
     public static boolean crearYagregarPiloto(Piloto piloto){
-
-        System.out.println(Files.exists(rutaBaseDatos));
-
         boolean agregado = false;
 
         String agregarPiloto = "INSERT INTO drivers (code, forename, surname, dob, nationality, url) VALUES (?,?,?,?,?,?)";
@@ -30,12 +25,115 @@ public class ConectarConBaseDatos {
             insercion.setString(5, piloto.getNationality());
             insercion.setString(6, piloto.getUrl());
 
+            insercion.executeUpdate();
+
             agregado = true;
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
         return agregado;
     }
 
+    public static Piloto leerPilotos(int id){
+
+        String consultaSQL = "SELECT * FROM drivers WHERE driverId = ?";
+
+        try(Connection conexion = DriverManager.getConnection("jdbc:sqlite:" + rutaBaseDatos.toString());
+            PreparedStatement consulta = conexion.prepareStatement(consultaSQL);){
+            consulta.setInt(1, id);
+            ResultSet resultado = consulta.executeQuery();
+            return new Piloto(resultado.getInt("driverId"),
+                    resultado.getString("code"),
+                    resultado.getString("forename"),
+                    resultado.getString("surname"),
+                    resultado.getString("dob"),
+                    resultado.getString("nationality"),
+                    resultado.getString("url"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static ArrayList<Piloto> leerPilotos(){
+        ArrayList<Piloto> pilotos = new ArrayList<>();
+        String consultaSQL = "SELECT * FROM drivers";
+
+        try (Connection conexion = DriverManager.getConnection("jdbc:sqlite:" + rutaBaseDatos.toString());
+             PreparedStatement consulta = conexion.prepareStatement(consultaSQL);){
+
+            ResultSet resultado = consulta.executeQuery();
+
+            while (resultado.next()){
+                pilotos.add(new Piloto(resultado.getInt("driverId"),
+                        resultado.getString("code"),
+                        resultado.getString("forename"),
+                        resultado.getString("surname"),
+                        resultado.getString("dob"),
+                        resultado.getString("nationality"),
+                        resultado.getString("url")));
+            }
+
+            return pilotos;
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean actualizarPiloto(Piloto piloto){
+        String consultaSQL = "UPDATE drivers SET code = ?, forename = ?, surname = ?, dob = ?, nationality = ?, url = ? WHERE driverId = ?";
+        try(Connection conexion = DriverManager.getConnection("jdbc:sqlite:" + rutaBaseDatos.toString());
+            PreparedStatement consulta = conexion.prepareStatement(consultaSQL);){
+
+            consulta.setString(1, piloto.getCode());
+            consulta.setString(2, piloto.getForename());
+            consulta.setString(3, piloto.getSurname());
+            consulta.setString(4, piloto.getDob());
+            consulta.setString(5, piloto.getNationality());
+            consulta.setString(6, piloto.getUrl());
+            consulta.setInt(7, piloto.getDriverId());
+
+            consulta.executeUpdate();
+
+            return true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean borrarPiloto(Piloto piloto){
+        String consultaSQL = "DELETE FROM drivers WHERE driverId = ?";
+        try(Connection conexion = DriverManager.getConnection("jdbc:sqlite:" + rutaBaseDatos.toString());
+            PreparedStatement consulta = conexion.prepareStatement(consultaSQL);){
+
+            consulta.setInt(1, piloto.getDriverId());
+
+            consulta.executeUpdate();
+
+            return true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void mostrarClasificacionPiloto(){
+        String consultaSQL = "SELECT d.code, sum(r.points) AS puntos FROM drivers d JOIN results r " +
+                "ON d.driverid = r.driverid GROUP BY d.code ORDER BY puntos DESC";
+
+        try(Connection conexion = DriverManager.getConnection("jdbc:sqlite:" + rutaBaseDatos.toString());
+            PreparedStatement consulta = conexion.prepareStatement(consultaSQL);){
+
+            ResultSet resultado = consulta.executeQuery();
+
+            while (resultado.next()){
+                System.out.println(resultado.getString("code") + " " + resultado.getInt("puntos"));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void mostrarClasificacionContructores(){
+        
+    }
 }
--- Tema 3/Ejercicio6_JDBCySQLite/src/main/java/org/example/Main.java
+++ Tema 3/Ejercicio6_JDBCySQLite/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -11,5 +13,27 @@ public class Main {
             System.out.println("No se pudo agregar el piloto");
         }
 
+
+        Piloto pilotoLeido = ConectarConBaseDatos.leerPilotos(3);
+        //System.out.println(pilotoLeido);
+
+        ArrayList<Piloto> pilotos = ConectarConBaseDatos.leerPilotos();
+        for (Piloto piloto1 : pilotos) {
+            System.out.println(piloto1);
+        }
+
+        if (ConectarConBaseDatos.actualizarPiloto(
+                new Piloto(9, "AAA", "AAA", "AAA", "1985-01-07", "British", "http://en.wikipedia.org/wiki/Lewis_Hamilton"))) {
+            System.out.println("Piloto actualizado");
+        } else {
+            System.out.println("No se pudo actualizar el piloto");
+        }
+
+        if (ConectarConBaseDatos.borrarPiloto(
+                new Piloto(9, "AAA", "AAA", "AAA", "1985-01-07", "British", "http://en.wikipedia.org/wiki/Lewis_Hamilton"))) {
+            System.out.println("Piloto eliminado");
+        }
+
+        ConectarConBaseDatos.mostrarClasificacionPiloto();
     }
 }
