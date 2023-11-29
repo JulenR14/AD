f793545991279f23d6526569d1a3c72e4ae40220
JulenR14 <144845185+JulenR14@users.noreply.github.com>
2023-11-29 16:15:11 +0100

    No funciona la conexion

--- Tema 3/Ejercicio9_MongoDB/src/main/java/org/example/Piloto.java
+++ Tema 3/Ejercicio9_MongoDB/src/main/java/org/example/Piloto.java
@@ -17,13 +17,15 @@ public class Piloto {
     private String dob;
     private String nationality;
     private String url;
+    private Constructor constructor;
 
-    public Piloto(String code, String forename, String surname, String dob, String nacionality, String url){
+    public Piloto(String code, String forename, String surname, String dob, String nacionality, String url, Constructor constructor){
         this.code = code;
         this.forename = forename;
         this.surname = surname;
         this.dob = dob;
         this.nationality = nacionality;
         this.url = url;
+        this.constructor = constructor;
     }
 }
--- Tema 3/Ejercicio9_MongoDB/src/main/java/org/example/db/ConectarConBaseDatos.java
+++ Tema 3/Ejercicio9_MongoDB/src/main/java/org/example/db/ConectarConBaseDatos.java
@@ -17,15 +17,17 @@ import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class ConectarConBaseDatos {
 
-    private String url = "mongodb://Adminjulen:1234@3.229.16.156:27017";
+    private static String url = "mongodb://Julen:1234@ec2-3-229-16-156.compute-1.amazonaws.com:27017";
 
-    public void pruebaConexion(){
+    public static void pruebaConexion(){
         try(MongoClient mongoClient = MongoClients.create(url)){
             CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
             CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
 
             MongoDatabase database = mongoClient.getDatabase("f1-2006").withCodecRegistry(pojoCodecRegistry);
             MongoCollection<Piloto> collection = database.getCollection("drivers", Piloto.class);
+
+            System.out.println("La cantidad de pilotos es : " + collection.countDocuments());
         }
     }
 
