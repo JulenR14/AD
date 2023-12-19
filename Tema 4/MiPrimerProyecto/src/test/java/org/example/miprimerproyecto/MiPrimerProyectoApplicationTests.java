54b379d623e1d064db88b7064131e81c42969ac5
JulenR14 <julen20191414@gmail.com>
2023-12-19 17:15:07 +0100

    Mi primer proyecto Spring

---
+++ Tema 4/MiPrimerProyecto/src/test/java/org/example/miprimerproyecto/MiPrimerProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.miprimerproyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimerProyectoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
